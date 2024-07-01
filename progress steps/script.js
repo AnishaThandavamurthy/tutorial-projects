07e5eeef4a3b9f176d1d46b502bbe7a2de323297
AnishaThandavamurthy <anishathandavamurthy@gmail.com>
2024-07-01 13:42:32 +0530

    adding my projects
    x

---
+++ progress steps/script.js
@@ -0,0 +1,59 @@
+const progress=document.getElementById('progress');
+const prev=document.getElementById('prev');
+const next=document.getElementById('next');
+const circles=document.querySelectorAll('.circle');
+console.log(next);
+
+var currentActive=1;
+next.addEventListener('click',() => {
+    currentActive++;
+    if(currentActive > circles.length){
+        currentActive =circles.length;
+    }
+    console.log(currentActive);
+    update();
+        
+    
+   
+})
+
+prev.addEventListener('click',() => {
+    currentActive--;
+    if(currentActive < 1){
+        currentActive =1;
+    }
+    console.log(currentActive);
+    update(); 
+    
+   
+})
+
+function update() {
+    //update Active class in circle
+    circles.forEach((circle,index) => {
+        if(index < currentActive){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    })
+
+    //update the progress 
+    var activeCircles = document.querySelectorAll('.active');
+    progress.style.width=((activeCircles.length -1) /(circles.length-1)*100) +'%';
+    
+    
+
+    //Change the button enabled and disabled state
+    if(currentActive ===1){
+        prev.disabled=true;
+    }
+    else if(currentActive === circles.length){
+        next.disabled=true;
+    }else
+    prev.disabled=false;
+    next.disabled=false;
+}
+
+
+
