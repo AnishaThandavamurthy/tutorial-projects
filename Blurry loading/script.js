c259e9d84803a2ec9449bf5195b68754d71eca54
AnishaThandavamurthy <anishathandavamurthy@gmail.com>
2024-07-07 21:29:41 +0530

    adding my projects

---
+++ Blurry loading/script.js
@@ -0,0 +1,22 @@
+const loadingtext = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+var load = 0;
+var int = setInterval(blurring , 30)
+function blurring () {
+    load++;
+    if(load> 99){
+        clearInterval(int);
+    }
+    console.log(loadingtext)
+    
+
+    loadingtext.innerHTML= `${load}%`;
+    loadingtext.style.opacity-scale(load, 0 ,100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`;
+
+}
+
+const scale = (num , in_min , in_max , out_min , out_max)=> {
+    return (num - in_min) * (out_max - out_min ) / (in_max - in_min ) +  out_min;
+}
